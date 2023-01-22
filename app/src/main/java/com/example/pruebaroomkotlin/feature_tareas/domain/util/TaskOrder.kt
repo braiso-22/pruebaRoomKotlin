194278913744e3bf0808a044e6cee64e83c273ca
Brais <braisfv22@gmail.com>
2023-01-22 19:30:46 +0100

    add copy function to taskOrder

--- app/src/main/java/com/example/pruebaroomkotlin/feature_tareas/domain/util/TaskOrder.kt
+++ app/src/main/java/com/example/pruebaroomkotlin/feature_tareas/domain/util/TaskOrder.kt
@@ -3,4 +3,11 @@ package com.example.pruebaroomkotlin.feature_tareas.domain.util
 sealed class TaskOrder(val orderType: OrderType){
     class Name(orderType: OrderType): TaskOrder(orderType)
     class Timestamp(orderType: OrderType): TaskOrder(orderType)
+
+    fun copy(orderType: OrderType): TaskOrder{
+        return when(this) {
+            is Name -> Name(orderType)
+            is Timestamp -> Timestamp(orderType)
+        }
+    }
 }
